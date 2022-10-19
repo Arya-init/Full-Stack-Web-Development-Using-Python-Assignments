7cbe6fe2dc3d7f7ea5e759039887ac097d4d1639
unknown <kharearya94@gmail.com>
2022-10-19 06:03:11 +0530

    assignment-1 added

---
+++ Assignment-1/10.py
@@ -0,0 +1,7 @@
+'''10. Write a python script to print values of three variables, each in a new line. Variable
+contains integer values.'''
+
+num1 = 35
+num2 = 7
+num3 = 21
+print(num1,num2,num3,sep = '\n')
---
+++ Assignment-1/9.py
@@ -0,0 +1,7 @@
+'''9. Write a python script to create variables to store your name, age, qualification, years 
+of experience and print it.'''
+
+name = "Arya"
+age = 27
+qualification = "MCA"
+print("Name: %s"%name,"Age: %d"%age,"Qualification: %s"%qualification,sep = "\n") 
